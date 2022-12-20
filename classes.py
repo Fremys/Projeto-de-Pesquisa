e9d8b37d699aead6f98ec53827a3bb530435adc2
Matheus Aguilar <matheus.pego@pattrol.com.br>
2022-12-19 22:42:01 -0300

    Add lista od buus stop for lines of bus

--- classes.py
+++ classes.py
@@ -1,4 +1,4 @@
-class PontoOnibus:
+class PontoOnibusLinha:
     # método construtor
     def __init__(self, id, id_ponto, cod_linha, nome_linha, nome_sublinha, origem, ident_ponto, geo):
         self.id = id
@@ -59,5 +59,57 @@ class PontoOnibus:
         
     def setGeo(self, geo):
         self.geo = geo
+
+class LinhasOnibus:
+    # método construtor
+    def __init__ (self, cod_linha, nome_linha, nome_sublinha, origem):
+        self.cod_linha = cod_linha
+        self.nome_linha = nome_linha
+        self.nome_sublinha = nome_sublinha
+        self.origem = origem
         
+    # Gets
+    def getCodLinha(self):
+        return self.cod_linha     
     
+    def getNomeSublinha(self):
+        return self.nome_sublinha
+    
+    def getNomeLinha(self):
+        return self.nome_linha
+    
+    def getOrigem(self):
+        return self.origem
+    
+    # Sets
+    def setCodLinha(self, cod_linha):
+        self.cod_linha = cod_linha
+    
+    def setNomeLinha(self, nome_linha):
+        self.nome_linha
+    
+    def setNomeSubLinha(self, nome_sub_linha):
+        self.nome_sublinha = nome_sub_linha
+        
+    def setOrigem(self, origem):
+        self.origem = origem
+        
+class PontoOnibus:
+    # método construtor
+    def __init__ (self, id, geometria):
+        self.id = id
+        self.geometria = geometria
+    
+    # Gets
+    def getId(self):
+        return self.id
+    
+    def getGeometria(self):
+        return self.geometria
+    
+    # Sets
+    def setId(self, id):
+        self.id = id
+        
+    def setGeometria(self, geometria):
+        self.geometria = geometria
--- main.py
+++ main.py
@@ -1,28 +1,44 @@
 # importar classe
+from classes import PontoOnibusLinha
 from classes import PontoOnibus
+from classes import LinhasOnibus
 import pandas as pd
 
 def data_process(filename):
+    # definir dados
+    _result = {}
+    
+    
     # ler arquivo de dados
     df = pd.read_csv(filename + ".csv")
     
-    # Lista de pontos
-    list_pontos = []
     
-    # Definir lista de colunas
+    # listar colunas
     columns = df.columns
     
     for i in range(0, len(df)):
-        ponto_onibus = PontoOnibus(df[columns[0]][i], df[columns[1]][i], df[columns[2]][i], df[columns[3]][i], df[columns[4]][i], df[columns[5]][i], df[columns[6]][i], df[columns[7]][i])
-        list_pontos.append(ponto_onibus)
-    
-    return list_pontos
-    
-    
+        ponto_onibus = PontoOnibusLinha(df[columns[0]][i], df[columns[1]][i], df[columns[2]][i], df[columns[3]][i], df[columns[4]][i], df[columns[5]][i], df[columns[6]][i], df[columns[7]][i])
+
+        _list_find = _result.get(ponto_onibus.getIdenPonto())
+        
+        if(_list_find != None):
+            _current_linha = LinhasOnibus( ponto_onibus.getCodLinha(), ponto_onibus.getNomeLinha(), ponto_onibus.getNomeSubLinha(), ponto_onibus.getOrigem())
+            _list_find.append(_current_linha)
+            _result[ponto_onibus.getIdenPonto()] = _list_find
+
+        else:
+            _list_linha = []
+            _current_linha = LinhasOnibus( ponto_onibus.getCodLinha(), ponto_onibus.getNomeLinha(), ponto_onibus.getNomeSubLinha(), ponto_onibus.getOrigem())
+            _list_linha.append(_current_linha)
+            _result[ponto_onibus.getIdenPonto()] = _list_linha       
+
+    return _result
+        
 
 
 # ===== Linha principal de execução ====
-data_process("data")
+result_pick = data_process("data")
+print("finish")
 
 
 
